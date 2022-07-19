0a23fd3897598b9ebc61c5ebbe6b6544986ce3a0
Chris Gick <chris.gick@thorntechnologies.com>
2022-07-19 15:51:29 -0400

    Update Test
    * Update test to load resources from packages
    
    Relates to SFTPG-2642

--- src/test/test_pgpdecrypt.py
+++ src/test/test_pgpdecrypt.py
@@ -1,15 +1,22 @@
 import unittest
+from importlib.resources import files
+
 import src.main.res.pgpDecrypt as pgpDecrypt
-import os
+import src.main.res.sharedconstants as sharedconstants
+
 
 class TestPgpDecrypt(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        os.chdir(os.path.split(__file__)[0])
-        with open('resource/keys/pgptestPrivate.asc', 'r') as key:
-            pgpDecrypt.import_gpg_key(key.read())
+        pgpDecrypt.import_gpg_key(
+            files('resource.keys').joinpath('pgptestPrivate.asc').read_text()
+        )
 
+    def test_key_was_imported(self):
+        keys = sharedconstants.gpg.list_keys()
+        uids = [uid for key in keys for uid in key.get('uids')]
+        self.assertIn("pgptest <pgptest@email.com>", uids)
 
     def test_decrypt(self):
         result = pgpDecrypt.decrypt('resource/test_file.txt.gpg')
