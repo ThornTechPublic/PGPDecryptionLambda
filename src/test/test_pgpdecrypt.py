a3de9a5516cba2da15b0b72ab5c6ba578b070208
Chris Gick <chris.gick@thorntechnologies.com>
2022-07-21 11:03:46 -0400

    Update PGP Decrypt Test
    * Add AWS event test
    
    Relates to SFTPG-2650

--- src/test/test_pgpdecrypt.py
+++ src/test/test_pgpdecrypt.py
@@ -1,6 +1,11 @@
+import json
 import unittest
 from importlib.resources import files
+from unittest import mock
 
+from aws_lambda_context import LambdaContext as context
+
+import src.main.AWS.aws_handler as aws_handler
 import src.main.res.pgpDecrypt as pgpDecrypt
 import src.main.res.sharedconstants as sharedconstants
 
@@ -21,6 +26,19 @@ class TestPgpDecrypt(unittest.TestCase):
     def test_decrypt(self):
         result = pgpDecrypt.decrypt('resource/test_file.txt.gpg')
         self.assertEqual(result.ok, True)
+
+    @mock.patch('src.main.AWS.aws_handler.os.remove')
+    @mock.patch.multiple('src.main.AWS.aws_handler',
+                         download_file_on_s3=mock.DEFAULT,
+                         download_asc_on_s3=mock.DEFAULT,
+                         copy_file_on_s3=mock.DEFAULT)
+    def test_s3_event(self, remove, download_file_on_s3, download_asc_on_s3, copy_file_on_s3):
+        download_file_on_s3.return_value = 'resource/test_file.txt.gpg'
+        s3event = files('event').joinpath('s3event.json').read_text()
+        aws_handler.invoke(json.loads(s3event), context)
+        download_file_on_s3.assert_called_with('chris-sftpgw-pgp-encrypted', 'test.txt.gpg')
+        remove.assert_called_with('resource/test_file.txt.gpg')
+        copy_file_on_s3.assert_called_with('/tmp/downloads/test_file.txt', None, 'test.txt')
 
 
 if __name__ == '__main__':
